d7e062b9777656f193f3b7705db707aabb2e017a
loki7even <lounes.behloul@supinfo.com>
2022-12-18 13:27:34 +0100

    auth

--- src/routers/authentication-router.js
+++ src/routers/authentication-router.js
@@ -1,13 +1,39 @@
 import express from "express";
-
 import { User } from "../mongo.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const rounds = 10;
+const secretToken = 'lounes-soufian'
 
 const router = express.Router();
+
+router.post('/inscription', (request, response) => {
+    bcrypt.hash(request.body.password, rounds, (error, hash) => {
+        if(error) response.status(500).json(error)
+        else {
+            const newUser = User({email: request.body.email, password: hash});
+            newUser.save()
+            .then(
+                user => {
+                    response.status(200).json({token: generateToken(user)})
+                }
+            )
+            .catch(
+                error => {
+                    response.status(500).json(error)
+                }
+            )
+        }
+    })
+});
+
+function generateToken(user) {
+    return jwt.sign({data: user}, secretToken, {expiresIn: '24h'})
+}
 
 router.post("/login", async (request, response) => {
     const log = await User.findOne()
 })
 
-router.post('/signup', (request, response) => {
-
-});
+export default router;
--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 
 import usersRouter from "./routers/users-router.js";
+import authRoute from "./routers/authentication-router";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/users", usersRouter);
+app.use("/api/auth", authRoute);
 
 export default app;
